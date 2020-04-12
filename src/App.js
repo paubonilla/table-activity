a5f2d987f2646d080d5ba0432dc6cb2672b8d391
Pau Bonilla <paulo111995@gmail.com>
2020-04-12 15:53:07 -0700

    created a table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Table } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Feature</Table.HeaderCell>
+            <Table.HeaderCell>Description</Table.HeaderCell>
+            <Table.HeaderCell>Action</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Show Debug</Table.Cell>
+            <Table.Cell>Description</Table.Cell>
+            <Table.Cell>Action</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Select Next Occurrence</Table.Cell>
+            <Table.Cell>Description</Table.Cell>
+            <Table.Cell>Action</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Show Source Control</Table.Cell>
+            <Table.Cell>Description</Table.Cell>
+            <Table.Cell>Action</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+
     </div>
   );
 }
